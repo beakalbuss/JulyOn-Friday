9df42c0c2a546082614c136f1073e685e6b59e72
beakalbuss <beakal.bus@gmail.com>
2020-07-03 10:12:05 -0400

    updates

---
+++ July/src/test/java/Check/Base.java
@@ -0,0 +1,22 @@
+package Check;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Base {
+	
+	
+	WebDriver driver;
+	@Test
+	public void PageObj(){
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\QA\\java\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Blue box");
+		driver.findElement(By.className("nav-input")).click();
+}
+}
